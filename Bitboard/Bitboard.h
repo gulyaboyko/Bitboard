57b14599caa71e13e73b85a11ac4ca60bc3594de
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-06-01 19:50:30 +0300

    Initial Commit

---
+++ Bitboard/Bitboard.h
@@ -0,0 +1,19 @@
+//
+//  Bitboard.h
+//  Bitboard
+//
+//  Created by Gulya Boiko on 6/1/20.
+//  Copyright © 2020 com.gulya.boiko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bitboard.
+FOUNDATION_EXPORT double BitboardVersionNumber;
+
+//! Project version string for Bitboard.
+FOUNDATION_EXPORT const unsigned char BitboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bitboard/PublicHeader.h>
+
+
